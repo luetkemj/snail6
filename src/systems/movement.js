d0bbcb45fd3399be2cf55b5148ed138c2e5a0c34
luetkemj <luetkemj@gmail.com>
2020-05-10 21:47:41 -0500

    fix: only create queries once per system

--- src/systems/movement.js
+++ src/systems/movement.js
@@ -2,11 +2,11 @@ import ecs from "../state/ecs";
 import MoveTo from "../components/MoveTo";
 import Position from "../components/Position";
 
-export const movement = () => {
-  const entities = ecs.createQuery({
-    all: [MoveTo, Position],
-  });
+const entities = ecs.createQuery({
+  all: [MoveTo, Position],
+});
 
+export const movement = () => {
   entities.get().forEach((entity) => {
     const mPos = {
       x: entity.position.x + entity.moveTo.x,
--- src/systems/render.js
+++ src/systems/render.js
@@ -3,13 +3,13 @@ import Appearance from "../components/Appearance";
 import Position from "../components/Position";
 import { clearCanvas, drawCell } from "../lib/canvas";
 
+const entities = ecs.createQuery({
+  all: [Appearance, Position],
+});
+
 export const render = () => {
   clearCanvas();
   // render map
-  const entities = ecs.createQuery({
-    all: [Appearance, Position],
-  });
-
   entities.get().forEach((entity) => {
     drawCell(entity);
   });
