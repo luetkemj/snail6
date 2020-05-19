1ed5ef9e5ed31929d7d833fe704c06ace4bca378
luetkemj <luetkemj@gmail.com>
2020-05-19 18:29:05 -0500

    removing comments

--- src/systems/fov.js
+++ src/systems/fov.js
@@ -15,9 +15,6 @@ export const fov = () => {
 
   const FOV = createFOV(opaqueEntities, width, height, originX, originY, 100);
 
-  // try doing this from FOV.fov and checking entities at location cache to speed up!
-  // console.log({ FOV });
-
   // clear out stale fov
   inFovEntities.get().forEach((x) => x.remove(IsInFov));
 
@@ -36,35 +33,5 @@ export const fov = () => {
         }
       });
     }
-    // cache.read("entitiesAtLocation", locId).forEach((eId) => {
-    //   const entity = ecs.getEntity(eId);
-    //   entity.add(IsInFov);
-
-    //   if (entity.light && entity.light.a > 0) {
-    //     if (!entity.isRevealed) {
-    //       entity.add(IsRevealed);
-    //     }
-    //   }
-    // });
   });
-
-  // renderableEntities.get().forEach((entity) => {
-  //   const locId = `${entity.position.x},${entity.position.y}`;
-
-  //   if (FOV.fov.has(locId)) {
-  //     if (!entity.isInFov) {
-  //       entity.add(IsInFov);
-  //     }
-
-  //     if (entity.light && entity.light.a > 0) {
-  //       if (!entity.isRevealed) {
-  //         entity.add(IsRevealed);
-  //       }
-  //     }
-  //   } else {
-  //     if (entity.isInFov) {
-  //       entity.remove(IsInFov);
-  //     }
-  //   }
-  // });
 };
